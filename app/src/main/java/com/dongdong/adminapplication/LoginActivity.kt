205efb2ddec19aa172aa3d20faf9270696a0425a
kimdongjae <rlaehdwo29@naver.com>
2023-04-03 00:27:08 +0900

    - init

---
+++ app/src/main/java/com/dongdong/adminapplication/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.dongdong.adminapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
